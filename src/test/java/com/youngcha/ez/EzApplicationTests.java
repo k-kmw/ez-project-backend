93923819c0176c7e4b91416722b67a6a5b690c61
k-kmw <kimmw2_@naver.com>
2024-06-14 18:18:56 +0900

    init

---
+++ src/test/java/com/youngcha/ez/EzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngcha.ez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
