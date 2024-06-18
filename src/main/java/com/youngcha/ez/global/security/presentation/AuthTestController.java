3f66c9d356ad1fdb25d4a418c12b6da1fcfab95e
k-kmw <kimmw2_@naver.com>
2024-06-18 13:38:36 +0900

    test: jwt 세션 정보 가져오기 테스트

--- src/main/java/com/youngcha/ez/global/security/presentation/AuthTestController.java
+++ src/main/java/com/youngcha/ez/global/security/presentation/AuthTestController.java
@@ -1,8 +1,14 @@
 package com.youngcha.ez.global.security.presentation;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class AuthTestController {
@@ -11,6 +17,16 @@ public class AuthTestController {
     @ResponseBody
     public String accessTestWithJWT() {
 
-        return "ok";
+        // jwt는 stateless로 관리되지만, 요청에 대하여 세션을 생성하고 응답할 때까지 유지하기 때문에 사용자 정보에 접근 가능
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "ok " + userId + " " + role;
     }
 }
