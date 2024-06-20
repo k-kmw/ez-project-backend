47a827fef2aa218d1aaa92a49c6d346f31283a0c
k-kmw <kimmw2_@naver.com>
2024-06-20 19:40:11 +0900

    fix: SecurityConfig requestMatcher base url 수정 auth -> user

--- src/main/java/com/youngcha/ez/global/security/config/SecurityConfig.java
+++ src/main/java/com/youngcha/ez/global/security/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
         // 경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/auth/**", "/").permitAll()
+                        .requestMatchers("/user/**", "/").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/user/reissue").permitAll()
                         .anyRequest().authenticated()
