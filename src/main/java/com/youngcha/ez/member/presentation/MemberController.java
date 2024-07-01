202c5fdb98fc3b4ec8ff20a70a945dbd8230600e
k-kmw <kimmw2_@naver.com>
2024-07-01 19:21:53 +0900

    fix: RequestBody 어노테이션 추가

--- src/main/java/com/youngcha/ez/member/presentation/MemberController.java
+++ src/main/java/com/youngcha/ez/member/presentation/MemberController.java
@@ -8,11 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -36,7 +32,7 @@ public class MemberController {
     }
 
     @PutMapping("/info")
-    public ResponseEntity<?> userInfoUpdate(@Valid MemberRequest.UserInfoUpdateDTO userInfoUpdateDTO)
+    public ResponseEntity<?> userInfoUpdate(@RequestBody @Valid MemberRequest.UserInfoUpdateDTO userInfoUpdateDTO)
             throws Exception {
 
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
