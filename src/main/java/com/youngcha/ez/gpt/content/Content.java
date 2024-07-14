7c38a4ec16ddb5fcca2874a56757dde08b5cc938
오동규 <ehdrb700606@naver.com>
2024-07-14 18:19:14 +0900

    feat: Content DTD

---
+++ src/main/java/com/youngcha/ez/gpt/content/Content.java
@@ -0,0 +1,25 @@
+package com.youngcha.ez.gpt.content;
+
+public class Content {
+
+    public Content(String type) {
+        this.type = type;
+    }
+
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Content{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
