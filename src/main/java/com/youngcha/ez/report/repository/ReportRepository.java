be930fc92fa0f61db3a2cad42abdcdeaa9ff180a
오동규 <ehdrb700606@naver.com>
2024-07-24 23:13:37 +0900

    feat: 리포트 목록(페이징) 조회 기능 작성

--- src/main/java/com/youngcha/ez/report/repository/ReportRepository.java
+++ src/main/java/com/youngcha/ez/report/repository/ReportRepository.java
@@ -1,7 +1,13 @@
 package com.youngcha.ez.report.repository;
 
 import com.youngcha.ez.report.dto.Report;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report,Long> {
+
+    public List<Report> findByNameContaining(Pageable pageable,String name);
+
 }
