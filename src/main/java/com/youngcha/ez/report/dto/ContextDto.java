8ed9de7211cfe0ddcbfee61e5e1d78b3bf9de80e
오동규 <ehdrb700606@naver.com>
2024-07-24 23:08:19 +0900

    feat: ContextDto 빌더패턴

--- src/main/java/com/youngcha/ez/report/dto/ContextDto.java
+++ src/main/java/com/youngcha/ez/report/dto/ContextDto.java
@@ -1,37 +1,16 @@
 package com.youngcha.ez.report.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContextDto {
     private Long contextId;
     private String title;
     private String content;
-
-    public Long getContextId() {
-        return contextId;
-    }
-
-    public void setContextId(Long contextId) {
-        this.contextId = contextId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public ContextDto(Long contextId, String title, String content) {
-        this.contextId = contextId;
-        this.title = title;
-        this.content = content;
-    }
 }
