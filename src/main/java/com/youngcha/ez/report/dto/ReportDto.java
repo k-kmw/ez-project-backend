4fa2c62925a2dc6a7d3695f81dc063e72078134f
오동규 <ehdrb700606@naver.com>
2024-07-24 23:13:11 +0900

    fix: reportDto 빌더 패턴 적용

--- src/main/java/com/youngcha/ez/report/dto/ReportDto.java
+++ src/main/java/com/youngcha/ez/report/dto/ReportDto.java
@@ -1,61 +1,20 @@
 package com.youngcha.ez.report.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReportDto{
     private long reportId;
     private String name;
     private String opinion;
     private String targetPrice;
     private List<ContextDto> contextList;
-
-    public ReportDto(long reportId, String name, String opinion, String targetPrice, List<ContextDto> contextList) {
-        this.reportId = reportId;
-        this.name = name;
-        this.opinion = opinion;
-        this.targetPrice = targetPrice;
-        this.contextList = contextList;
-    }
-
-    public long getReportId() {
-        return reportId;
-    }
-
-    public void setReportId(long reportId) {
-        this.reportId = reportId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getOpinion() {
-        return opinion;
-    }
-
-    public void setOpinion(String opinion) {
-        this.opinion = opinion;
-    }
-
-    public String getTargetPrice() {
-        return targetPrice;
-    }
-
-    public void setTargetPrice(String targetPrice) {
-        this.targetPrice = targetPrice;
-    }
-
-    public List<ContextDto> getContextList() {
-        return contextList;
-    }
-
-    public void setContextList(List<ContextDto> contextList) {
-        this.contextList = contextList;
-    }
-
-
 }
